4d2e4f7e331cf9828fa4e28722ac210a18b835ce
zevik7 <zevik2807@gmail.com>
2022-10-17 14:19:08 +0700

    feat: home screen

---
+++ src/screens/Home.screen.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { MoodPicker } from '../components/MoodPicker';
+import { MoodOptionType, MoodOptionWithTimestamp } from '../type';
+
+export const Home: React.FC = () => {
+  const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
+
+  const handleSelectMood = React.useCallback((mood: MoodOptionType) => {
+    setMoodList(current => [...current, { mood, timestamp: Date.now() }]);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MoodPicker onSelect={handleSelectMood} />
+
+      {moodList.map(item => (
+        <Text key={item.timestamp}>
+          {item.mood.emoji} {new Date(item.timestamp).toString()}
+        </Text>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
